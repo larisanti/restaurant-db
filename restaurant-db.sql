6aec451907c81fba39097785169d0b22848665bd
larisanti <desantilarissa@gmail.com>
2025-01-25 08:57:13 -0300

    Update

--- restaurant-db.sql
+++ restaurant-db.sql
@@ -1,14 +1,8 @@
 -- Import MySQL Connector/Python
 -- Create database and tables for Restaurant
 
--- Check if the database exists, and delete if it does
-DROP DATABASE IF EXISTS little_lemon_db;
-
--- Create the database
-CREATE DATABASE little_lemon_db;
-
--- Switch to the newly created database
-USE little_lemon_db;
+CREATE DATABASE restaurant-db;
+USE restaurant-db;
 
 -- Create MenuItems table
 CREATE TABLE MenuItems (
@@ -114,12 +108,12 @@ INSERT INTO Orders (OrderID, TableNo, MenuID, BookingID, Quantity, BillAmount) V
 
 -- Insert data into Employees table
 INSERT INTO Employees (EmployeeID, Name, Role, Address, Contact_Number, Email, Annual_Salary) VALUES
-(1, 'Mario Gollini', 'Manager', '724, Parsley Lane, Old Town, Chicago, IL', 351258074, 'Mario.g@littlelemon.com', 70000),
-(2, 'Adrian Gollini', 'Assistant Manager', '334, Dill Square, Lincoln Park, Chicago, IL', 351474048, 'Adrian.g@littlelemon.com', 65000),
-(3, 'Giorgos Dioudis', 'Head Chef', '879 Sage Street, West Loop, Chicago, IL', 351970582, 'Giorgos.d@littlelemon.com', 50000),
-(4, 'Fatma Kaya', 'Assistant Chef', '132 Bay Lane, Chicago, IL', 351963569, 'Fatma.k@littlelemon.com', 45000),
-(5, 'Elena Salvai', 'Head Waiter', '989 Thyme Square, EdgeWater, Chicago, IL', 351074198, 'Elena.s@littlelemon.com', 40000),
-(6, 'John Millar', 'Receptionist', '245 Dill Square, Lincoln Park, Chicago, IL', 351584508, 'John.m@littlelemon.com', 35000);
+(1, 'Mario Gollini', 'Manager', '724, Parsley Lane, Old Town, Chicago, IL', 351258074, 'Mario.g@restaurant.com', 70000),
+(2, 'Adrian Gollini', 'Assistant Manager', '334, Dill Square, Lincoln Park, Chicago, IL', 351474048, 'Adrian.g@restaurant.com', 65000),
+(3, 'Giorgos Dioudis', 'Head Chef', '879 Sage Street, West Loop, Chicago, IL', 351970582, 'Giorgos.d@restaurant.com', 50000),
+(4, 'Fatma Kaya', 'Assistant Chef', '132 Bay Lane, Chicago, IL', 351963569, 'Fatma.k@restaurant.com', 45000),
+(5, 'Elena Salvai', 'Head Waiter', '989 Thyme Square, EdgeWater, Chicago, IL', 351074198, 'Elena.s@restaurant.com', 40000),
+(6, 'John Millar', 'Receptionist', '245 Dill Square, Lincoln Park, Chicago, IL', 351584508, 'John.m@restaurant.com', 35000);
 
 -- Create stored procedure for PeakHours
 DELIMITER //
